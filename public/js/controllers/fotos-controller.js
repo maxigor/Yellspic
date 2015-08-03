a6ec8d9ea6b100867294005debc3a9a9a75e4bd8
Max Igor <maxigor.ferreira@gmail.com>
2015-08-03 00:52:20 -0300

    That's just how I roll

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,21 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+	$scope.fotos = [];
+
+	$http.get('v1/fotos')
+	.success(function(dataFotos){ // dataFotos são as fotos que o $http.get pegou do link ou local
+		console.log(dataFotos);
+		$scope.fotos = dataFotos; // incliu o dataFotos dentro do $scope
+	})
+	.error(function(erro){
+		console.log(erro);
+	})
+
+/*
+	var promise = $http.get('v1/fotos'); //pega as imagens do link ou local
+	promise.then(function(retorno){ // retorna as imagens para a varialvel quando estiver pronto
+		$scope.fotos = retorno.data; //callback com as imagens (data)
+	}).catch(function(error){ // retornar erro caso aconteça algo
+		console.log(error); //mostra erro no console
+	});
+*/
+});
